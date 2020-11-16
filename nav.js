50c3a5e31a7385575b57c70d8462cf2f24a18002
Hassan Mahmood <72471842+Hmahmood01@users.noreply.github.com>
2020-11-16 12:31:15 +0500

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,26 @@
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.navlist');
+  const navLinks = document.querySelectorAll('.navlist li');
+
+
+
+  burger.addEventListener ('click', () => {
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+      if(link.style.animation){
+        link.style.animation = '';
+      }
+      else{
+        link.style.animation = 'navLinksFade 0.5s ease forwards ${index / 5 + 0.5}s';
+      }
+    });
+
+    burger.classList.toggle('toggle');
+
+  });
+}
+
+
+navSlide();
